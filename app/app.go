cf59b55d391ddc29812ca3e52e64a7e54cf97120
anderson.caldeira <anderson.caldeira@novaxs.com.br>
2022-03-01 13:57:43 -0300

    Adding command line process

--- app/app.go
+++ app/app.go
@@ -1,11 +1,45 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Generate will prepare the application to be executed
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Find by IP"
 	app.Usage = "Find IP and names of servers in internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Find IP and names of servers in internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
+
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
